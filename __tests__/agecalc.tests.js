6d1181160c16e14b68df8a5b1f9730ae4700d383
Celeste Morton <creneemorton@gmail.com>
2019-11-15 14:08:26 -0800

    test the third spec and fail

--- __tests__/agecalc.tests.js
+++ __tests__/agecalc.tests.js
@@ -2,14 +2,22 @@ import {
     Human
 } from './../src/human.js';
 
+
 describe('Human', () => {
 
     test('should calculate humans age in mercury years only', () => {
-        const newHuman = new Human();
+        let newHuman = new Human(31);
+        newHuman.planetAge();
         expect(newHuman.ageOnMercury).toEqual(129);
     });
     test('should calculate humans age in venus years only', () => {
-        const newHuman = new Human();
-        expect(newHuman.ageOnVenus).toEqual(41);
+        let newHuman = new Human(31);
+        newHuman.planetAge();
+        expect(newHuman.ageOnVenus).toEqual(50);
+    });
+    test('should calculate humans age in mars years only', () => {
+        let newHuman = new Human(31);
+        newHuman.planetAge();
+        expect(newHuman.ageOnMars).toEqual(16);
     });
 });
