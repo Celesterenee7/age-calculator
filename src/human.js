9e8fb735f3d4b693053e8a007057927cc4ab8163
Celeste Morton <creneemorton@gmail.com>
2019-11-15 14:17:07 -0800

    test the fourth spec and pass

--- src/human.js
+++ src/human.js
@@ -7,5 +7,6 @@ export class Human {
     this.ageOnMercury = parseFloat((this.age / .24).toFixed(0));
     this.ageOnVenus = parseFloat((this.age / .62).toFixed(0));
     this.ageOnMars = parseFloat((this.age / 1.88).toFixed(0));
+    this.ageOnJupiter = parseFloat((this.age / 11.86).toFixed(0));
   }
 }
