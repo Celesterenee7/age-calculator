72b94f898d0c9391ed217d328b8032b17ae41f7a
Celeste Morton <creneemorton@gmail.com>
2019-11-15 14:24:46 -0800

    test the 5th spec and fail

--- __tests__/agecalc.tests.js
+++ __tests__/agecalc.tests.js
@@ -25,4 +25,16 @@ describe('Human', () => {
         newHuman.planetAge();
         expect(newHuman.ageOnJupiter).toEqual(3);
     });
+    test('should calculate humans age in saturn years only', () => {
+        let newHuman = new Human(31);
+        newHuman.planetAge();
+        expect(newHuman.ageOnSaturn).toEqual(1);
+    });
+    // test('should calculate life expectancy for each planet', () => {
+    //     let newHuman = new Human(31);
+    //     expect(newHuman.ageOnMercury).toEqual(129);
+    //     expect(newHuman.ageOnVenus).toEqual(129);
+    //     expect(newHuman.ageOnMars).toEqual(129);
+    //     expect(newHuman.ageOnJupiter).toEqual(129);
+    // });
 });
