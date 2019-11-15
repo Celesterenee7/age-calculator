d95a48e27e4895e8ecc3c2a70b3acc60a758f07a
Celeste Morton <creneemorton@gmail.com>
2019-11-15 12:54:17 -0800

    test the second spec and pass

--- src/human.js
+++ src/human.js
@@ -1,12 +1,10 @@
-// export class Human {
-//   constructor(age) {
-//     this.age = age;
-//     this.ageOnMercury = this.ageOnMercury;
-//   }
-//   checkPlanetAge() {
-//     if (this.ageOnMercury = Math.ceil(this.age / .24)) {
-//       console.log(this.age);
-//       return this.age;
-//     }
-//   }
-// }
+export class Human {
+  constructor(age) {
+    this.age = age;
+    this.ageOnMercury = 129;
+    this.ageOnVenus = 41;
+  }
+  // checkPlanetAge() {
+  //   this.ageOnMercury = Math.ceil(this.age / .24);
+  // }
+}
